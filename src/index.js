92b67f6aaee806ffb8e7d67c2eee08f24d253c9f
Sanches-23 <pasechnik23.2002@gmail.com>
2023-09-13 22:11:05 +0300

    want BEM methodology

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './components/styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      {/*<HashRouter>*/}
-      {/*</HashRouter>*/}
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
 
